7ba1fa365e88045c6c083e20fe924f2ee4611183
R8S <Roosevelt.bseg@gmail.com>
2024-01-29 23:25:27 -0500

    404

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Roosevelt\'s Blog";
+export const SITE_TITLE = "El Contemplador";
 export const SITE_DESCRIPTION = 'Demo site for porfolio';
 export const HOMEPAGE_ARTICLE_LIMIT = 7;
 export const ARTICLES_PER_PAGE = 6;
