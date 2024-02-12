7c3d1721057f07ff4c61adfb270e204f2fb874f2
R8S <Roosevelt.bseg@gmail.com>
2024-02-11 23:20:33 -0500

    finished pagination

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "El Contemplador";
 export const SITE_DESCRIPTION = 'Demo site for porfolio';
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
-export const ARTICLES_PER_PAGE = 6;
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const ARTICLES_PER_PAGE = 3;
--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -19,8 +19,32 @@ export const GET: APIRoute = async ({ url }): Promise<Response> => {
       }
     );
   }
-};
 
-const allBlogArticles: CollectionEntry<'blog'>[] = await getCollection(
-    'blog'
-);
+  const allBlogArticles: CollectionEntry<"blog">[] = await getCollection(
+    "blog"
+  );
+
+  //Filter articles based on query
+  const searchResult = allBlogArticles.filter((article) => {
+    const titleMatch: boolean = article.data.title
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+
+    const BodyMatch: boolean = article.body
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+
+    const slugMatch: boolean = article.slug
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+
+    return titleMatch || BodyMatch || slugMatch;
+  });
+
+  return new Response(JSON.stringify(searchResult), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
